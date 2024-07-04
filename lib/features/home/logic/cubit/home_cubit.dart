2e3e51dd40472636db8fbc5f0f5e4e9e72495f04
Mohamed Abdo <eng.mohamedabdo139@gmail.com>
2024-07-04 23:43:15 +0300

    -addedhome api constants file -added home response model -added home api service -added home repo -added home cubit & home states -added dI for home feature

---
+++ lib/features/home/logic/cubit/home_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:mydoctor_app/features/home/data/repos/home_repo.dart';
+
+import 'home_state.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  HomeCubit(this._homeRepo) : super(const HomeState.initial());
+  final HomeRepo _homeRepo;
+  void specializationLoading() async {
+    emit(const HomeState.specializationLoading());
+    final response = await _homeRepo.getSpecialization();
+    response.when(
+        success: (specializationsResponseModel) {
+          emit(HomeState.specializationSuccess(specializationsResponseModel));
+        },
+        failure: (errorHandler) {
+          emit(HomeState.specializationError(errorHandler));
+        },
+
+    );  
+    
+  }
+}
